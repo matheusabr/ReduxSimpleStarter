31b4d2c5284f2c7f2dc949f67b7fef340f3316fb
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 00:20:29 -0300

    chore(index): fetch videos from youtube api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Youtube search
+import YTSearch from 'youtube-api-search';
+
 // Components
 import SearchBar from './components/search_bar';
 
 // Youtube API Key
 const YOUTUBE_API_KEY = 'AIzaSyDLSKC85B8smihsmEoYEK-W7Ma2Xo6divc';
+YTSearch({key: YOUTUBE_API_KEY, term: 'csgo'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component
 // This component should produce some HTML
