ddccc470d5dc0698e47865154179eba67f66b45b
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 01:28:18 -0300

    chore(video_list_item): populate the list of videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+  // Instead of:
+  // const VideoListItem = (props) => {
+  //   const video = props.video;
+  // We can simply use the solution below
+  // to get a specific props value
+const VideoListItem = ({ video }) => {
+  const videoSnippet = video.snippet;
+  const imageUrl = videoSnippet.thumbnails.default.url;
+  const heading = videoSnippet.title;
+
   return (
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {heading}
+          </div>
+        </div>
+      </div>
+    </li>
   );
 }
 
