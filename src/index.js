80cf5bd32f4cd8dedae50156fc9cb41777d00b0d
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-03 23:09:15 -0300

    chore(): add youtube-api-search lib

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 // Components
 import SearchBar from './components/search_bar';
 
+// Youtube API Key
+const YOUTUBE_API_KEY = 'AIzaSyDLSKC85B8smihsmEoYEK-W7Ma2Xo6divc';
+
 // Create a new component
 // This component should produce some HTML
 const App = () => {
