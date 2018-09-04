0a4cb716c734df7dac3a34fd86942c2932a69fca
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 15:30:51 -0300

    feat: select video to play

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,7 +6,12 @@ const VideoList = (props) => {
   // re-render only the updated video
   // of list
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video}  />
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}  />
+    );
   });
 
   return (
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -5,13 +5,13 @@ import React from 'react';
   //   const video = props.video;
   // We can simply use the solution below
   // to get a specific props value
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   const videoSnippet = video.snippet;
   const imageUrl = videoSnippet.thumbnails.default.url;
   const title = videoSnippet.title;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
