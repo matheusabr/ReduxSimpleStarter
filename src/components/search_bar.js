3290cc544f4e349c53f2ef9b7508e5eae48794ba
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 00:24:38 -0300

    fix(search): move comments to outside render function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,12 +7,12 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
+  // To really update input value
+  // we need to set to it the
+  // new state value
   render() {
     return (
       <div>
-        // To really update input value
-        // we need to set to it the 
-        // new state value
         <input
           value={this.state.term}
           onChange={event => this.setState( { term: event.target.value })} />
