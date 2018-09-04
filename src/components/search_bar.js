c12815e88c16d7a9699541c5fe006ebcb533e0df
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 00:12:03 -0300

    chore(search): update input value based on state value changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  // To use and init state we need
-  // to use a constructor of the class
-  // and set state as a plain object
   constructor(props) {
-    // super calls the own constructor
-    // of React.Component
     super(props);
-    // Initialize state
+
     this.state = { term: '' };
   }
 
   render() {
     return (
       <div>
-        // Updating a state by using setState
-        // Warning: Do not update by direct access
-        // like this.stater.term
-        <input onChange={event => this.setState( { term: event.target.value })} />
-        // Accessing a state
-        My name is {this.state.term}
+        // To really update input value
+        // we need to set to it the 
+        // new state value
+        <input
+          value={this.state.term}
+          onChange={event => this.setState( { term: event.target.value })} />
       </div>
     );
   }
