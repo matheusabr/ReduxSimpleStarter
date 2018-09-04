dfd997f0883b3041f80c6d7dfb15a8d7a255165d
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-03 23:16:33 -0300

    doc(search): comment component type

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,8 @@
 // the JSX to React.createElement
 import React from 'react';
 
+// It is called React Functional component
+// cause it is a function
 const SearchBar = () => {
   return <input />;
 };
