0bd44e8c8801a3f16d5fdc502ecef081c4af1675
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 15:37:54 -0300

    style(): set css values to each component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
   // new state value
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={event => this.setState( { term: event.target.value })} />
