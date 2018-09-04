e99a6a8ed6b7fc230f9f5010bd2fc6d7d54d06e3
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-04 00:34:22 -0300

    chore(index): change to class component + set and update state

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 // Youtube search
 import YTSearch from 'youtube-api-search';
-
 // Components
 import SearchBar from './components/search_bar';
-
 // Youtube API Key
 const YOUTUBE_API_KEY = 'AIzaSyDLSKC85B8smihsmEoYEK-W7Ma2Xo6divc';
-YTSearch({key: YOUTUBE_API_KEY, term: 'csgo'}, function(data) {
-  console.log(data);
-});
 
-// Create a new component
-// This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'csgo'}, (videos) => {
+      // Update state with retrieved videos list
+      console.log(videos);
+      // this.setState({ videos: videos });
+      // When the key and value have the same
+      // name we can simply use a single name
+      // to set its value:
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component`s generated HTML
-// and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
