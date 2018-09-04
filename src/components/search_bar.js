2c7ec782685150af51a87fabc233afc170f098c9
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-03 23:31:24 -0300

    refactor(search): change functional to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,15 @@
-// We need to import ReactDOM
-// cause it will transpile
-// the JSX to React.createElement
-import React from 'react';
+import React, { Component } from 'react';
+// Using React, { Component } is
+// the same of set a const e.g:
+// const Component = React.Component;
+// then it`ll be available to use just Component
 
-// It is called React Functional component
-// cause it is a function
-const SearchBar = () => {
-  return <input />;
-};
+// It is called React class component
+// and it extends a React component
+class SearchBar extends Component {
+  render() {
+    return <input />
+  }
+}
 
 export default SearchBar;
